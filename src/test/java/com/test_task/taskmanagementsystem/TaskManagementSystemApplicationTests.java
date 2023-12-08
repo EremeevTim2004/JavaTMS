c951306c71fa5c610a669e446172d69a49e8d5c4
ForestGump <74667646+EremeevTim2004@users.noreply.github.com>
2023-12-08 13:46:59 -0500

    Initiated the project using Spring Initializer
    initiated the project using Spring Initializer with dependencies: Maven, Java 21, PosgreSQL, Spring Web, Spring Data JPA & Spring Security

---
+++ src/test/java/com/test_task/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_task.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
